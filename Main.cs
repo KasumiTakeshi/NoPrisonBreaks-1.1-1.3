fd25787fc8589dc1d0615adccc2f41cb95c8e801
KasumiTakeshi <55848562+KasumiTakeshi@users.noreply.github.com>
2020-03-04 16:02:41 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+using System.Reflection;
+
+namespace KTNoPrisonBreaks
+{
+    [StaticConstructorOnStartup]
+    public static class NoPrisonBreaks
+    {
+        static NoPrisonBreaks()
+        {
+            var harmony = new Harmony("com.noprisonbreaks.patch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(PrisonBreakUtility), "StartPrisonBreak", new[] { typeof(Pawn), typeof(string), typeof(string), typeof(LetterDef) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out })]
+        class PrisonBreakPatch
+        {
+            [HarmonyPrefix]
+            static bool PrisonBreakPrefix()
+            {
+                Log.Message("Prison break suppressed by KasumiTakeshi's No Prison Breaks [1.1]");
+                return false;
+            }
+        }
+    }
+}
